5c32db73d8a3e1923243ac4d1846bc1078307f28
Franciane <franciane.penina@gmail.com>
2022-06-17 02:20:05 -0300

    feat: add POST to /tweets

--- index.js
+++ index.js
@@ -9,9 +9,18 @@ app.use(express.json());
 const user = [{username: "teste", 
               avatar: "https://img.freepik.com/fotos-gratis/3d-rendem-de-uma-mesa-de-madeira-com-uma-imagem-defocussed-de-um-barco-em-um-lago_1048-3432.jpg?w=2000"}];
 
+
+const tweets = [{username: "teste"},
+                {tweet:"só testanto"}]
+
 app.post("/sign-up", (req, res) => {
     user.push(req.body);
     res.send("OK");            
+});
+
+app.post("/tweets", (req, res) => {
+    tweets.push(req.body);
+    res.send("OK");
 });
             
 app.listen(5000);
